3f1fb6c3bf4636df09aed9f259f09dbb56e6d616
Edgar Solis <edgar21dejunio@gmail.com>
2018-01-17 15:17:33 -0600

    Remove Linux Support
    It is now working on Windows XP so there's no longer any need
    for it to work on linux, or at least for the time being
    there isn't.
    
    If in the future there is such need then It will have
    to be added back because right now it is too much of a
    problem.

--- Printer.py
+++ Printer.py
@@ -14,17 +14,11 @@ class Print(object):
         pass
 
     def Print(self, dialog, simplified=None, other=None):
-        """Print."""
-        try:
-            self.printLinux(dialog)
-        except ValueError:
-            self.PrintWindows(dialog, simplified=None, other=None)
-        else:
-            pass
-
-    def PrintWindows(self, dialog, simplified=None, other=None):
         """Print the passed dialog."""
         printer = QPrinter(QPrinter.HighResolution)
+        printer.isValid()
+        printer
+        prinfo = QPrinterInfo(printer)
         if simplified:
             printer.setDocName("COMANDA")
         elif other:
@@ -49,48 +43,3 @@ class Print(object):
         painter.translate(-1 * dialog.width() / 2, -1 * dialog.height() / 2)
         dialog.render(painter)
         painter.end()
-
-    def printLinux(self, dialog):
-        """Print the passed dialog.
-
-        Linux doesn't have raspberry pi drivers for the printer, so if running
-        on a raspberry pi print with this method
-        """
-        painter = QPainter()
-
-        p = dialog.palette()
-        p.setColor(dialog.backgroundRole(), Qt.white)
-
-        dialog.setPalette(p)
-
-        width = dialog.getSize()[0]
-
-        height = dialog.getSize()[1]
-
-        scale = 2.2
-
-        pixmap = QImage(width * scale, height * scale, QImage.Format_Grayscale8)
-
-        painter.begin(pixmap)
-        painter.scale(scale, scale)
-        dialog.render(painter)
-        painter.end()
-
-        buffer = QBuffer()
-        buffer.open(QIODevice.ReadWrite)
-        pixmap.save(buffer, "PNG")
-        pixmap.save("Hi.png", "PNG")  # just for testing
-
-        strio = io.BytesIO()
-        strio.write(buffer.data())
-        buffer.close()
-        strio.seek(0)
-        pil_im = Image.open(strio)
-
-        buffer = None
-
-        p = Usb(0x04b8, 0x0e03, 0)
-        p.image
-        p.set(align='center')
-        p.image(pil_im, impl="bitImageRaster", fragment_height=3000)
-        p.cut()
