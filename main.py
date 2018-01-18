960cbd7bf45a4d1f9285acd56212378b4745c89a
Edgar Solis <edgar21dejunio@gmail.com>
2018-01-18 17:23:02 -0600

    Simple formatting

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ import logging
 import os
 import traceback
 
+path = os.path.realpath(__file__)
+# Configure logger to write to a file...
+logging.basicConfig(filename=path + '.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger = logging.getLogger()
+
+
+def my_handler(type, value, tb):
+    """Exception loger format."""
+    tr = traceback.extract_tb(tb)
+    trace = ""
+    for row in tr:
+        trace += str(row) + "\n"
+    logger.exception("""Uncaught exception: {} | value {} | trace: \n{}""".format(str(value), value, trace))
+
+
+# Install exception handler
+sys.excepthook = my_handler
+
 
 class MainWindow(QWidget):
     """Main window widget."""
@@ -31,25 +50,6 @@ class MainWindow(QWidget):
         p.setColor(self.backgroundRole(), Qt.white)
         self.setPalette(p)
 
-
-path = os.path.realpath(__file__)
-# Configure logger to write to a file...
-logging.basicConfig(filename=path + '.log', level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
-logger = logging.getLogger()
-
-
-def my_handler(type, value, tb):
-    """Exception loger format."""
-    tr = traceback.extract_stack(tb)
-    trace = ""
-    for row in tr:
-        trace += str(row) + "\n"
-    logger.exception("""Uncaught exception: {} | value {} | trace: \n{}""".format(str(value), value, trace))
-
-
-# Install exception handler
-sys.excepthook = my_handler
 
 app = QApplication(sys.argv)
 window = MainWindow()
