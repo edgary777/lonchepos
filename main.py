fe50a746f1661af05f9d0322219e2aa0cbe09737
Edgar Solis <edgar21dejunio@gmail.com>
2018-01-18 18:32:13 -0600

    Logging didn't work after freezing.

--- main.py
+++ main.py
@@ -6,25 +6,6 @@ import logging
 import os
 import traceback
 
-path = os.path.realpath(__file__)
-# Configure logger to write to a file...
-logging.basicConfig(filename=path + '.log', level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
-logger = logging.getLogger()
-
-
-def my_handler(type, value, tb):
-    """Exception loger format."""
-    tr = traceback.extract_tb(tb)
-    trace = ""
-    for row in tr:
-        trace += str(row) + "\n"
-    logger.exception("""Uncaught exception: {} | value {} | trace: \n{}""".format(str(value), value, trace))
-
-
-# Install exception handler
-sys.excepthook = my_handler
-
 
 class MainWindow(QWidget):
     """Main window widget."""
