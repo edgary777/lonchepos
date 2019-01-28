e53c137403a2f9e4b3645a7d69fea280c306293b
Edgar Solis <edgar21dejunio@gmail.com>
2019-01-28 12:15:28 -0600

    Error Logging
    Modified:
      - Modified main.py to log errors to a file
          when frozen.

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 import sys
+import os
+import atexit
+import logging
+import inspect
 from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout
 from PyQt5.QtCore import Qt
 import Session
+
+# Logging errors to a file should only happen when the program has been freezed.
+# The logging will only be used when the file path and python path are the same.
+
+# Find the file path and split it to a list.
+filePath = inspect.stack()[0][1].split('/')
+# Remove the file from the path to get the directory.
+del filePath[len(filePath) - 1]
+# Turn the path into a string again.
+filePath = "/".join(filePath)
+
+if os.path.dirname(sys.executable) == filePath:
+
+    logging.basicConfig(filmoveename=os.path.dirname(sys.executable) + '/errors.log',
+                        level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(name)s %(message)s')
+    logger = logging.getLogger('mylogger')
+
+    def my_handler(type, value, tb):
+        """Error handler."""
+        logger.exception("Uncaught exception: {0}".format(str(value)))
+
+    sys.excepthook = my_handler
 
 
 class MainWindow(QWidget):
